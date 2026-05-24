e597c65953d833a9addd088a696d063c066fa7f8
ThangG <nguyenmanhthang2k5@gmail.com>
2026-05-24 17:03:08 +0700

    oknhe

--- config/config.php
+++ config/config.php
@@ -7,9 +7,17 @@ function pdo_get_connection(){
     $password = 'AVNS_-1SrybZyko7wPHyDjfx'; 
 
     $dburl = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+    
+    // THAY ĐỔI ĐOẠN NÀY: Bỏ qua việc đọc file ca.pem vật lý để tránh lỗi đường dẫn trên Linux
     $options = array(
-        PDO::MYSQL_ATTR_SSL_CA => __DIR__ . '/ca.pem', 
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     );
-    return new PDO($dburl, $username, $password, $options);
+
+    try {
+        return new PDO($dburl, $username, $password, $options);
+    } catch (PDOException $e) {
+        // Bẫy lỗi thông minh để không làm sập server gây lỗi 502 nữa
+        die("<h3>Lỗi kết nối CSDL Cloud:</h3> <p>" . $e->getMessage() . "</p>");
+    }
 }
